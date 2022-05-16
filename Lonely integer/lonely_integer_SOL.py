14b6aa0252ce3a502aa9167c5ce4893476286cff
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-05-16 02:56:11 +0200

    Create lonely_integer_SOL.py

---
+++ Lonely integer/lonely_integer_SOL.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+def lonelyinteger(a):
+    for i in a:
+        if a.count(i)==1:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
